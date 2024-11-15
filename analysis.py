b928be7a00099038ebd7e15ed0ea946c52e4b565
Dong Ha's MBP <2dongha.lee@gmail.com>
2024-11-15 11:20:08 +0000

    added analysis

--- analysis.py
+++ analysis.py
@@ -1,13 +1,14 @@
-import getdist
-import matplotlib.pyplot as plt
-from getdist import plots
 import os
 
-# specify chain file root
-chain_root = "./spt-tau-chains/lcdm"
+import getdist
+import matplotlib.pyplot as plt
+import numpy as np
+from getdist import plots
 
-# list interested parameters
-params = ["As", "ns", "ombh2"]
+# specify chain file root
+# chain_root = "./spt-tau-chains/lcdm_comp"
+# chain_root = "./act-tau-chains/lcdm"
+chain_root = "./act-wmap-chains/lcdm"
 
 # directory and base name
 chain_dir = os.path.dirname(chain_root)
@@ -15,10 +16,29 @@ chain_base = os.path.basename(chain_root)
 
 # outputs directory
 outputs_dir = os.path.join(chain_dir, "outputs")
-os.makedirs(output_dir, exist_ok=True)
+os.makedirs(outputs_dir, exist_ok=True)
 
 # load MCMC chains
 samples = getdist.loadMCSamples(chain_root, settings={"ignore_rows": 0.3})
+
+# adding Ase2tau derived parameter
+
+# Extract parameter arrays
+As = samples.getParams().As
+tau = samples.getParams().tau
+
+# Calculate the derived parameter: 1e9 * As * exp(-2 * tau)
+derived_param = 1e9 * As * np.exp(-2 * tau)
+
+# Add the derived parameter to the samples
+samples.addDerived(derived_param, name="Ase2tau", label="10^9 A_s e^{-2\\tau}")
+
+# list interested parameters
+# spt_tau
+# params = ["H0", "ombh2", "omch2", "theta_MC_100", "Ase2tau", "ns"]
+
+# act_tau
+params = ["H0", "ombh2", "omch2", "theta_MC_100", "tau", "ns", "logA"]
 
 # printing R-1 value
 print("R-1 =", samples.getGelmanRubin())
@@ -35,10 +55,10 @@ plt.savefig(fig_path, dpi=300)
 print(f"triangle plot saved at {fig_path}")
 
 # Write marginalized parameters with uncertainties to a file
-params_path = os.path.join(output_dir, chain_base + "_parameters.txt")
+params_path = os.path.join(outputs_dir, chain_base + "_parameters.txt")
 with open(params_path, "w") as f:
     f.write("R-1 = {:.5f}\n".format(samples.getGelmanRubin()))
     f.write("\nMarginalized parameters with uncertainties:\n")
     for param in params:
         f.write("{} = {}\n".format(param, param_latex[param]))
-print(f"parameters saved at {parameters_file_path}")
+print(f"parameters saved at {params_path}")
